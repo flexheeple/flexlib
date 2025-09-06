f70bc7b4559bd807e8a543a741f4062e5620cdef
flexheeple <flexheeple@gmail.com>
2025-09-06 09:33:19 +0700

    Account for header in csv file

--- fcsv.h
+++ fcsv.h
@@ -12,10 +12,11 @@ typedef struct fcsv_row {
 typedef struct fcsv {
     fsv_t      parse_point;
     fsb_t      content;
+    fcsv_row_t header;
     fcsv_row_t rows;
 } fcsv_t;
 
-bool fcsv_open(fcsv_t *csv, const char *file_path);
+bool fcsv_open(fcsv_t *csv, const char *file_path, bool have_header);
 void fcsv_close(fcsv_t *csv);
 bool fcsv_get_next_column(fsv_t *row, fsv_t *column);
 bool fcsv_get_next_row(fcsv_t *csv, fcsv_row_t *out);
@@ -25,10 +26,10 @@ bool fcsv_get_next_row(fcsv_t *csv, fcsv_row_t *out);
 int main(void) {
     fcsv_t csv = {0};
     const char *file_path = "../fcsv-parser/cashew.csv";
-    if (!fcsv_open(&csv, file_path)) return 1;
+    if (!fcsv_open(&csv, file_path, true)) return 1;
 
-    fcsv_row_t *row = NULL;
-    while (fcsv_get_next_row(&csv, NULL)) {
+    fcsv_row_t row;
+    while (fcsv_get_next_row(&csv, &row)) {
         row = &csv.rows;
         for (size_t i = 0; i < row->size; ++i) {
             if (row->datas[i].length != 0 && row->datas[i].datas != NULL) {
@@ -48,7 +49,7 @@ int main(void) {
 
 #ifdef FCSV_IMPLEMENTATION
 
-bool fcsv_open(fcsv_t *csv, const char *file_path) {
+bool fcsv_open(fcsv_t *csv, const char *file_path, bool have_header) {
     if (!fsb_read_entire_file(file_path, &csv->content)) {
         FSV_LOGE("[FCSV] Couldn't open file `%s`. %s\n", file_path, strerror(errno));
         return false;
@@ -61,7 +62,10 @@ bool fcsv_open(fcsv_t *csv, const char *file_path) {
         fsv_t row           = {};
         fsv_t column        = {};
         if (!fsv_split_by_cstr(&csv->parse_point, "\r\n", false, &row)) return false;
-        while (fcsv_get_next_column(&row, &column)) { column_count++; }
+        while (fcsv_get_next_column(&row, &column)) {
+            if (have_header) { fda_append(&csv->header, column); }
+            column_count++;
+        }
         fda_reserve(&csv->rows, column_count);
     }
 
